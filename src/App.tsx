4256a15c38357eb53cb997d5107277f284b2bf08
Siddardha-ch <siddhusiddhu@gmail.com>
2025-12-27 17:38:57 +0530

    fixed nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { HashRouter, Routes, Route } from "react-router-dom";
-
 
 const queryClient = new QueryClient();
 
@@ -15,12 +14,11 @@ const App = () => (
       <Toaster />
       <Sonner />
       <HashRouter>
-  <Routes>
-    <Route path="/" element={<Index />} />
-    <Route path="*" element={<Index />} />
-  </Routes>
-</HashRouter>
-
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
